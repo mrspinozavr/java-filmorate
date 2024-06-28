43ac5a375fe4d5bbd842ad1d22010946e3735e94
Aleksandr Osiptsov <solamer@yandex.ru>
2024-06-28 13:32:37 +0300

    Решение 10 ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -25,7 +25,7 @@ public class FilmController {
     }
 
     @PostMapping
-    public Film create(@Valid  @RequestBody Film film){
+    public Film create(@Valid  @RequestBody Film film) {
         log.info("Создание фильма {}", film);
         film.setId(generateId());
         films.put(film.getId(), film);
